3e22aad9136a2b22689aecd5048903f3928a94c2
ChenglongMa <mcl.cn@outlook.com>
2023-02-07 18:54:56 +1100

    Ignore error when running multiple models

--- run_multi_models.py
+++ run_multi_models.py
@@ -9,24 +9,37 @@ from recbole_debias.quick_start import run_recbole_debias
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--models', '-m', nargs='+', type=str, default=['MF'], help='name of models')
-    parser.add_argument('--dataset', '-d', type=str, default='ml-100k', help='name of datasets')
+    parser.add_argument('--dataset', '-d', type=str, default='kwai', help='name of datasets')
     parser.add_argument('--config_files', '-c', type=str, default=None, help='config files')
+    parser.add_argument('--model_files', '-f', nargs='+', type=str, default=None, help='model checkpoint files')
 
     args, _ = parser.parse_known_args()
+    models = args.models
+    mfs = args.model_files
+    model_and_files = []
+    for i in range(len(mfs)):
+        mf = mfs[i]
+        model = models[i]
+        if mf.split('-')[0].lower() != model.lower():
+            raise TypeError("Checkpoint file does not match model name")
+        model_and_files.append((model, f"saved/{mf}.pth" if mf is not None else None))
 
     config_file_list = args.config_files.strip().split(' ') if args.config_files else None
     res = []
 
-    for model in args.models:
+    for model, model_file in model_and_files:
         start = time.time()
-        result = run_recbole_debias(model=model, dataset=args.dataset, config_file_list=config_file_list)
-        elapse = (time.time() - start) / 60  # unit: s
-        for metric, value in result['test_result'].items():
-            res.append([model, metric, value, elapse])
+        try:
+            result = run_recbole_debias(model=model, dataset=args.dataset, config_file_list=config_file_list, model_file=model_file)
+            elapse = (time.time() - start) / 60  # unit: s
+            for metric, value in result['test_result'].items():
+                res.append([model, metric, value, elapse])
+        except:
+            print('Error')
+            pass
 
     res = pd.DataFrame(res, columns=['model', 'metric', 'value', 'elapse(mins)'])
     os.makedirs('./result/', exist_ok=True)
     now = time.strftime("%y%m%d%H%M%S")
     res.to_csv(f'./result/result_{now}.csv', index=False)
     print(res.head())
-
